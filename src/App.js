a73e07a4d1fed41190a66c98097407953b4e2a17
Ravi <ravigunz@gmail.com>
2023-05-06 12:32:23 +0530

    fetch data using axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts?userId=1")
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-      })
-      .catch((e) => {});
-  }, []);
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-      })
-      .catch((e) => {});
-  }, []);
+  // fetch
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/posts?userId=1")
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       console.log(json);
+  //     })
+  //     .catch((e) => {});
+  // }, []);
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/todos")
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       console.log(json);
+  //     })
+  //     .catch((e) => {});
+  // }, []);
+
+// fetch data usin axios
+useEffect(() => {
+  axios.get('https://jsonplaceholder.typicode.com/users').then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.log(error);
+  }).finally(function () {});
+}, []);
+
+useEffect(() => {
+  axios.get('https://jsonplaceholder.typicode.com/posts').then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.log(error);
+  }).finally(function () {});
+}, []);
 
   return (
     <div className="App">
