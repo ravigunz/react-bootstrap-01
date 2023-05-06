f1417715583e26a6b95a22753369d6263e5e701b
Ravi <ravigunz@gmail.com>
2023-05-06 12:17:25 +0530

    fetch data without using axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts?userId=1")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+      })
+      .catch((e) => {});
+  }, []);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+      })
+      .catch((e) => {});
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello React Bootstrap</h1>
